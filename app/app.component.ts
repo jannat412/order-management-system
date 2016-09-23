2ef36c8f5ef7745b448161b3ff5fab00c40f17f2
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-23 18:56:10 +0200

    add interpolation in template

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,8 @@ import { Component }         from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular 2 App</h1>'
+    template: '<h1>{{ title }}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string = 'My first Angular 2 App';
+}
