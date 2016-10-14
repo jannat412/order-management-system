2367a177597c62df2041c773cb9d109832746352
Carlos Matheu Armengol <info@plastikaweb.com>
2016-10-14 19:20:41 +0200

    general style fixes

--- src/app/products/filter-list/filter-list.component.ts
+++ src/app/products/filter-list/filter-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, Input, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
 
 @Component({
   selector: 'oms-filter-list',
