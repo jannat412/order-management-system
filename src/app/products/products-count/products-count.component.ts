c3aa8eede8a946a6d655e67de5075f4f9166bb2a
Carlos Matheu Armengol <info@plastikaweb.com>
2016-10-19 17:22:17 +0200

    products-count component init

---
+++ src/app/products/products-count/products-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oms-products-count',
+  templateUrl: './products-count.component.html',
+  styleUrls: ['products-count.component.scss']
+})
+export class ProductsCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
