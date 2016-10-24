80744c834af0962f8fe14ae93965d1316c004e4c
Carlos Matheu Armengol <info@plastikaweb.com>
2016-10-24 18:33:17 +0200

    categories filter reset button

--- src/app/products/category-filter-menu/category-filter-menu.component.ts
+++ src/app/products/category-filter-menu/category-filter-menu.component.ts
@@ -10,30 +10,25 @@ import {ICategory} from '../category';
 export class CategoryFilterMenuComponent implements OnInit {
   private categories: ICategory[];
   private errorMessage: string;
-  private listFilter: string;
+  private listFilter: string = '';
   @Output() private filterBy: EventEmitter<string>;
-  @Output() private filterByActive: EventEmitter<boolean>;
-  activeFilterStatus: boolean = true;
+  // @Output() private filterByActive: EventEmitter<boolean>;
+  // activeFilterStatus: boolean = true;
 
   constructor(private productsService: ProductsService) {
     this.filterBy = new EventEmitter<string>();
-    this.filterByActive = new EventEmitter<boolean>();
+    // this.filterByActive = new EventEmitter<boolean>();
   }
 
-  filterName = (): void => this.filterBy.emit( this.listFilter );
-
   onFilter = (category: string) => {
-    this.filterBy.emit( category );
+    this.listFilter = category;
+    this.filterBy.emit( this.listFilter );
   };
-  onFilterByActive = () => {
-    this.activeFilterStatus = !this.activeFilterStatus;
-    this.filterByActive.emit( this.activeFilterStatus );
-  };
+  // onFilterByActive = () => {
+  //   this.activeFilterStatus = !this.activeFilterStatus;
+  //   this.filterByActive.emit( this.activeFilterStatus );
+  // };
 
-  clear = (): void => {
-    this.listFilter = '';
-    this.filterName();
-  };
 
   ngOnInit() {
     this.productsService.getCategories()
