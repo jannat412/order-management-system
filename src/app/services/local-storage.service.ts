e034773fbf82fe7a7a398a231e31474c13a298f6
Carlos Matheu Armengol <info@plastikaweb.com>
2016-11-22 17:13:51 +0100

    local storage service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class LocalStorageService {
+
+    private ls;
+
+    constructor() {
+        this.ls = window.localStorage;
+    }
+
+    existsKey = (key: string) => ls[key] !== 'undefined';
+
+    getValue = (key: string): any => {
+        let returnValue;
+
+        if (this.existsKey( key )) {
+            const value = ls[key];
+
+            try {
+                returnValue = JSON.parse( value );
+            } catch (e) {
+                if (e.constructor.name === 'SyntaxError') {
+                    returnValue = value;
+                }
+            }
+        }
+
+        return returnValue;
+    };
+
+    saveValue = (key: string, value: any) => {
+        if (typeof value === 'object') {
+            this.ls[key] = JSON.stringify(value);
+        } else {
+            this.ls[key] = val;
+        }
+    };
+
+}
