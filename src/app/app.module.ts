cc64fe356266db198abad07c227dc0e810641186
Plastikaweb <info@plastikaweb.com>
2016-12-12 18:35:14 +0100

    add animate.css library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,10 @@ import {MenuCollapseDirective} from './directives/menu-collapse.directive';
 import {AuthService} from './services/auth.service';
 import {UserService} from './services/user.service';
 import {ConfigService} from './services/config.service';
+import {OrderService} from './services/order.service';
 
 import {AuthModule} from './auth-module/auth.module';
 import {HomeComponent} from './home/home.component';
-import {OrderService} from './services/order.service';
 
 @NgModule( {
     declarations: [
