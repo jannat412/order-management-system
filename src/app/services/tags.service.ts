284587189c2f271b69c62e51afb204e9c1554ebe
Plastikaweb <info@plastikaweb.com>
2016-12-13 21:21:49 +0100

    fix tags service

--- src/app/services/tags.service.ts
+++ src/app/services/tags.service.ts
@@ -1,24 +1,22 @@
 import {Injectable} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2';
 import {ITag} from '../models/tag';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class TagsService {
 
     constructor(private db: AngularFireDatabase) {}
 
-    /**
-     * get tag labels for a product
-     * @param productKey
-     * @returns {Observable<R>}
-     */
-    getTagsForProduct = (productKey: string): Observable<ITag[]> => {
+    getTagsForProduct(productKey: string): Observable<ITag[]> {
+
         return this.db.list( `tagsPerProduct/${productKey}` )
             .map( tags => tags
-                .map( tag => this.db.object( `tags/${tag.$key}` )) )
-            .flatMap( (tags) => Observable.combineLatest( tags ) );
-    };
+                .map( tag => {
+                    return this.db.object( `tags/${tag.$key}` );
+                } ) )
+            .mergeMap( (tags) => Observable.combineLatest( tags ) );
+
+    }
+
 }
