c82e88023b31af8b16831f7086130115793740c2
Plastikaweb <info@plastikaweb.com>
2016-12-16 18:54:36 +0100

    inactive guard refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './auth-module/login/login.component';
 import {HomeComponent} from './home/home.component';
 import {AuthGuard} from './services/auth.guard';
+import {InactiveGuard} from './services/inactive.guard';
 
 const APP_ROUTES: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-    {path: 'comanda', canActivateChild: [AuthGuard], loadChildren: 'app/order-module/order.module#OrderModule'},
+    {path: 'comanda', canActivateChild: [AuthGuard, InactiveGuard], loadChildren: 'app/order-module/order.module#OrderModule'},
     {path: 'admin', canActivateChild: [AuthGuard], loadChildren: 'app/admin-module/admin.module#AdminModule'},
-    {path: '**', redirectTo: '/login'}
+    {path: '**', redirectTo: '/home'}
 ];
 
 export const mainRouting = RouterModule.forRoot( APP_ROUTES );
--- src/app/services/inactive.guard.ts
+++ src/app/services/inactive.guard.ts
@@ -1,24 +1,29 @@
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
 import {ConfigService} from './config.service';
 
 @Injectable()
-export class InactiveGuard implements CanActivate {
+export class InactiveGuard implements CanActivate, CanActivateChild {
 
     result: boolean = false;
 
-    constructor(private configService: ConfigService, private router: Router) {}
+    constructor(private configService: ConfigService, private router: Router) {
+    }
 
     canActivate(route: ActivatedRouteSnapshot,
                 state: RouterStateSnapshot): Observable<boolean> | boolean {
-        this.configService.getActive()
-            .subscribe( result => {
-                this.result = result;
-                if (!this.result) {
-                    this.router.navigate( ['/home'] );
-                }
-            } );
-        return this.result;
+
+        return this.configService.getActive()
+            .take(1)
+            .map((activeState) => !!activeState)
+            .do(active => {
+                if (!active) this.router.navigate(['/home']);
+            });
+
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+        return this.canActivate( route, state );
     }
 }
