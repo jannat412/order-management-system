fd21e8b30c62a3bdb85ed53691f59adaf8f24700
Plastikaweb <info@plastikaweb.com>
2016-12-21 20:15:40 +0100

    product list item component

---
+++ src/app/order-module/products-list/product-list-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {CategoriesService} from '../../services/categories.service';
+import {IProduct} from '../../models/product';
+import {ICategory} from '../../models/category';
+
+@Component({
+  selector: '.oms-product-list-item',
+  templateUrl: './product-list-item.component.html'
+})
+export class ProductListItemComponent implements OnInit {
+  @Input() product: IProduct;
+  private category: ICategory;
+
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit() {
+    this.categoriesService.getCategoryForProduct(this.product.categoryKey)
+        .subscribe(
+            (data: ICategory) => this.category = <ICategory>data
+        );
+  }
+
+  getThumbUrl = (): string => {
+    return `/assets/product-img/thumbs/${this.product.imgName}`;
+  };
+
+}
--- src/app/order-module/products-list/products-list.component.ts
+++ src/app/order-module/products-list/products-list.component.ts
@@ -1,20 +1,14 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-
 import {ProductsService} from '../../services/products.service';
-import {CategoriesService} from '../../services/categories.service';
-
 import {IProduct} from '../../models/product';
-import {ICategory} from '../../models/category';
 import {NameFilterInputComponent} from '../name-filter-input/name-filter-input.component';
 import {CategoryFilterMenuComponent} from '../category-filter-menu/category-filter-menu.component';
-
 
 @Component( {
     selector: 'oms-order-list',
     templateUrl: 'products-list.component.html'
 } )
 export class ProductsListComponent implements OnInit {
-    private categories: ICategory[];
     private products: IProduct[];
     private errorMessage: string;
 
@@ -29,35 +23,15 @@ export class ProductsListComponent implements OnInit {
     private activeFilter: boolean = true;
     private selectedFilter: boolean = false;
 
-    constructor(private productsService: ProductsService,
-                private categoriesService: CategoriesService) {}
+    constructor(private productsService: ProductsService) {}
 
     ngOnInit() {
-
-        this.categoriesService.getCategories()
-            .subscribe(
-                (data: ICategory[]) => this.categories = <ICategory[]>data,
-                (error: any) => this.errorMessage = <any>error
-            );
         this.productsService.getProducts()
             .subscribe(
                 (data: IProduct[]) => this.products = <IProduct[]>data,
                 (error: any) => this.errorMessage = <any>error
             );
     }
-
-    getCategoryClass = (category: string): string => {
-        for (let cat of this.categories) {
-            if (cat.$key === category) {
-                return cat.className;
-            }
-        }
-        return '';
-    };
-
-    getThumbUrl = (thumb: string): string => {
-        return `/assets/product-img/thumbs/${thumb}`;
-    };
 
     doFilter = (str: string) => {
         this.listFilter = str;
