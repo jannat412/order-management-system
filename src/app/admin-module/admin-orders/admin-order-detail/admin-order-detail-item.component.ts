6a8d1f952bdb4923c3ffb6b48e26eb0624dec078
Plastikaweb <info@plastikaweb.com>
2017-01-09 19:49:31 +0100

    order by user row with counter

--- src/app/admin-module/admin-orders/admin-order-detail/admin-order-detail-item.component.ts
+++ src/app/admin-module/admin-orders/admin-order-detail/admin-order-detail-item.component.ts
@@ -1,28 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IProduct} from '../../../models/product';
 import {ProductsService} from '../../../services/products.service';
 import {Subscription} from 'rxjs/Subscription';
+import {ICounterData} from '../../../models/counterData';
 
-@Component({
-  selector: '.oms-admin-order-detail-list-item',
-  templateUrl: './admin-order-detail-item.component.html'
-})
+@Component( {
+    selector: '.oms-admin-order-detail-list-item',
+    templateUrl: './admin-order-detail-item.component.html'
+} )
 export class AdminOrderDetailItemComponent implements OnInit {
-  @Input() orderLine: any;
-  private product: IProduct;
-  @Input() index: number;
-  private productSubscription: Subscription;
+    @Input() orderLine: any;
+    private product: IProduct;
+    @Input() index: number;
+    private productSubscription: Subscription;
 
-  constructor(private productsService: ProductsService) { }
+    constructor(private productsService: ProductsService) {}
 
-  ngOnInit() {
-      this.productSubscription = this.productsService
-          .getProduct(this.orderLine.key, 'thumbs')
-          .subscribe(
-              (product: IProduct) => {
-                this.product = <IProduct>product;
-              }
-          );
-  }
+    ngOnInit() {
+        this.productSubscription = this.productsService
+            .getProduct( this.orderLine.$key, 'thumbs' )
+            .subscribe(
+                (product: IProduct) => this.product = <IProduct>product
+            );
+    }
+
+    createCounterData = (): ICounterData => {
+        return {
+            valuePerUnit: this.product.price,
+            quantity: this.orderLine.quantity,
+            step: 0.05,
+            totalMeasurementUnit: '€',
+            total: this.orderLine.total
+        };
+    };
+
+    updateOrderProductLine = (counterData: any) => {
+        console.log(counterData);
+    };
 
 }
